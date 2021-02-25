0a1e65ff08d864354e83fa11a5350f1b5cbdb9a3
Anderson Santos <anderson.a.santos@accenture.com>
2021-02-25 17:17:18 -0300

    feat: conclusão do curso

---
+++ src/data/usecases/db-authentication.ts
@@ -0,0 +1,30 @@
+import { Authentication } from '@/domain/usecases'
+import { HashComparer, Encrypter, LoadAccountByEmailRepository, UpdateAccessTokenRepository } from '@/data/protocols'
+
+export class DbAuthentication implements Authentication {
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
+  ) {}
+
+  async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
+
+    if (!account) return null
+
+    const { id, name, password } = account
+
+    const isValid = await this.hashComparer.compare(authenticationParams.password, password)
+
+    if (!isValid) return null
+
+    const accessToken = await this.encrypter.encrypt(id)
+    await this.updateAccessTokenRepository.updateAccessToken(id, accessToken)
+    return {
+      accessToken,
+      name
+    }
+  }
+}
